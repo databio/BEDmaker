fa4f760ce67efcfbcc1306210745901795ea91d5
joseverdezoto <j.esiempreliga@hotmail.com>
2019-11-08 15:50:18 -0500

    add bigwig cmd and make bedmaker looper compatible

--- bed_maker_update.py
+++ bed_maker_update.py
@@ -12,16 +12,18 @@ parser.add_argument("-c", "--chip-exp", help="is it a ChIP-Seq TF experiment or 
 parser.add_argument("-t", "--input-type", help="a bigwig or a bedgraph file that will be converted into BED format")
 parser.add_argument("-o", "--outfolder", default="output", help="folder to put the converted BED files in")
 
-parser = pypiper.add_pypiper_args(parser, groups=["pypiper", "common", "looper"],
- 											required=["--input-file", "--input-type", "--chip-exp"])
 
-args = parser.parse_args()  # all pipeline arguments should be defined by now
+# add pypiper args to make pipeline looper compatible
+parser = pypiper.add_pypiper_args(parser, groups=["pypiper", "looper"],
+                                            required=["--input-file", "--input-type", "--chip-exp", "--outfolder"])
+
+args = parser.parse_args()  
 
 # commands templates
 # bedGraph to bed
 bedGraph_template = "macs2 bdgpeakcall -i {input} -o {output}"
-bigwig_template = "bigWigToBedgraph -i {input} | macs2 -o {output}"
 # bigWig to bed
+bigwig_template = "bigWigToBedgraph -i {input} | macs2 bdgpeakcall -i {intermediate}  -o {output}"
 # big_wig_cmd = ""
 
 pm = pypiper.PipelineManager(name="bed_maker", outfolder=args.outfolder, args=args) #args defined with ArgParser and add_pypiper_args
@@ -39,11 +41,19 @@ def get_bed_path(current_path, outfolder):
     file_id = os.path.splitext(file_name)[0]
     return os.path.join(outfolder, file_id + ".bed")
 
+# Define target folder for converted files
+target = get_bed_path(args.input_file, args.outfolder)
 
-if args.input_type == "bedGraph":
-    print("Converting {} to BED format".format(args.input_file))
-    target = get_bed_path(args.input_file, args.outfolder)
-    cmd = bedGraph_template.format(input=args.input_file, output=target)
+
+if args.chip_exp == True:
+    if args.input_type == "bedGraph":
+        print("Converting {} to BED format".format(args.input_file))
+        cmd = bedGraph_template.format(input=args.input_file, output=target)
+    elif args.input_type == "bigWig":
+        print("Converting {} to BED format".format(args.input_file))
+        cmd = bigwig_template.format(input=args.input_file, 
+                                    intermediate=os.path.splitext(os.path.basename(args.input_file))[0] + ".bedgraph",  
+                                    output=target)
 else:
     raise NotImplementedError("Other conversions are not supported yet")
 
