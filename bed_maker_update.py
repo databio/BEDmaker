c07183bdca079f27b2cf4611ad8a688b228d52c4
joseverdezoto <j.esiempreliga@hotmail.com>
2019-11-12 15:48:37 -0500

    add new conversion templates

--- bed_maker_update.py
+++ bed_maker_update.py
@@ -8,7 +8,7 @@ parser = ArgumentParser(description = "A pipeline to convert bigwig or bedgraph 
 
 
 parser.add_argument("-f", "--input-file", help="path to the input file", type=str)
-parser.add_argument("-c", "--chip-exp", help="is it a ChIP-Seq TF experiment or a Histone modification ChiP-Seq experiment", type=str)
+parser.add_argument("-c", "--chip-exp", help="is it a ChIP-Seq TF experiment or a Histone modification ChiP-Seq experiment", type=bool)
 parser.add_argument("-t", "--input-type", help="a bigwig or a bedgraph file that will be converted into BED format")
 parser.add_argument("-o", "--outfolder", default="output", help="folder to put the converted BED files in")
 
@@ -19,11 +19,19 @@ parser = pypiper.add_pypiper_args(parser, groups=["pypiper", "looper"],
 
 args = parser.parse_args()  
 
-# commands templates
-# bedGraph to bed
-bedGraph_template = "macs2 bdgpeakcall -i {input} -o {output}"
-# bigWig to bed
-bigwig_template = "bigWigToBedgraph -i {input} | macs2 bdgpeakcall -i {intermediate}  -o {output}"
+# COMMANDS TEMPLATES
+
+# bedGraph to bed (narrowpeaks)
+bedGraph_narrow_template = "macs2 bdgpeakcall -i {input} -o {output}"
+# preliminary bigWig to bed
+bigWig_template = "bigWigToBedgraph {input} {intermediate} | macs2 bdgpeakcall -i {intermediate}  -o {output}"
+# bigBed to bed
+bigBed_template = "bigBedToBed {input} {output}"
+# bedgraph to bed (broadpeaks)
+bedGraph_broad_template = "macs2 bdgbroadcall -i {input} -o {output}"
+# preliminary bigWig to bed (broad peaks)
+bigWig_broad_template = "bigWigToBedgraph {input} {intermediate} | macs2 bdgbroadcall -i {intermediate}  -o {output}"
+
 # big_wig_cmd = ""
 
 pm = pypiper.PipelineManager(name="bed_maker", outfolder=args.outfolder, args=args) #args defined with ArgParser and add_pypiper_args
@@ -41,19 +49,36 @@ def get_bed_path(current_path, outfolder):
     file_id = os.path.splitext(file_name)[0]
     return os.path.join(outfolder, file_id + ".bed")
 
-# Define target folder for converted files
+# Define target folder for converted files and implement the conversions; True=TF_Chipseq False=Histone_Chipseq  
 target = get_bed_path(args.input_file, args.outfolder)
 
 
 if args.chip_exp == True:
     if args.input_type == "bedGraph":
         print("Converting {} to BED format".format(args.input_file))
-        cmd = bedGraph_template.format(input=args.input_file, output=target)
+        cmd = bedGraph_narrow_template.format(input=args.input_file, output=target)
     elif args.input_type == "bigWig":
         print("Converting {} to BED format".format(args.input_file))
-        cmd = bigwig_template.format(input=args.input_file, 
-                                    intermediate=os.path.splitext(os.path.basename(args.input_file))[0] + ".bedgraph",  
+        cmd = bigWig_template.format(input=args.input_file, 
+                                    #get the file basename
+                                    intermediate=os.path.splitext(os.path.basename(args.input_file))[0] + ".bedgraph",   
                                     output=target)
+    elif args.input_type == "bigBed":
+        print("Converting {} to BED format".format(args.input_file))
+        cmd = bigBed_template.format(input=args.input_file, output=target)
+elif args.chip_exp == False:
+    if args.input_type == "bedGraph":
+        print("Converting {} to BED format".format(args.input_file))
+        cmd = bedGraph_broad_template.format(input=args.input_file, output=target)
+    elif args.input_type == "bigWig":
+        print("Converting {} to BED format".format(args.input_file))
+        cmd = bigWig_broad_template.format(input=args.input_file, 
+                                    #get the file basename
+                                    intermediate=os.path.splitext(os.path.basename(args.input_file))[0] + ".bedgraph",   
+                                    output=target)
+    elif args.input_type == "bigBed":
+        print("Converting {} to BED format".format(args.input_file))
+        cmd = bigBed_template.format(input=args.input_file, output=target)
 else:
     raise NotImplementedError("Other conversions are not supported yet")
 
