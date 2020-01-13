e7ca3953e32df6f6800e8145a2065556a5a9416c
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2020-01-13 15:28:35 -0500

    add output dir checks and creation

--- bed_maker.py
+++ bed_maker.py
@@ -8,7 +8,7 @@ import pyBigWig
 from refgenconf import RefGenConf as RGC, select_genome_config, RefgenconfError, CFG_ENV_VARS, CFG_FOLDER_KEY
 import yacman
 
-parser = ArgumentParser(description = "A pipeline to convert bigwig or bedgraph files into bed format")
+parser = ArgumentParser(description="A pipeline to convert bigwig or bedgraph files into bed format")
 
 
 parser.add_argument("-f", "--input-file", help="path to the input file", type=str)
@@ -22,7 +22,7 @@ parser.add_argument("-o", "--output-file", help="path to the output BED files", 
 
 # add pypiper args to make pipeline looper compatible
 parser = pypiper.add_pypiper_args(parser, groups=["pypiper", "looper"],
-                                            required=["--input-file", "--input-type", "--chip-exp"])
+                                  required=["--input-file", "--input-type", "--chip-exp"])
 
 args = parser.parse_args()
 
@@ -43,33 +43,23 @@ bed_template = "ln -s {input} {output}"
 
 # SET OUTPUT FOLDERS
 # use output parent argument from looper 
-out_parent = args.output_parent 
-
-# def get_bed_path(current_path, outfolder):
-#     """
-#     Swap the file extension and change the directory
-
-#     :param str current_path: current path to the file to be converted
-#     :param str outfolder: output directory to place the file with the swapped extension in
-#     :return str: path to the file with swapped extension
-#     """
-#     file_name = os.path.basename(current_path)
-#     file_id = os.path.splitext(file_name)[0]
-#     return os.path.join(outfolder, file_id + ".bed")
+out_parent = args.output_parent
 
 file_name = os.path.basename(args.input_file)
 file_id = os.path.splitext(file_name)[0]
 sample_folder = os.path.join(out_parent, file_id) #specific output folder for each sample log and stats
+
+bed_parent = os.path.dirname(args.output_file)
+if not os.path.exists(bed_parent):
+    print("Output directory does not exist. Creating: {}".format(bed_parent))
+    os.makedirs(bed_parent)
 
 
 def main():
     pm = pypiper.PipelineManager(name="bed_maker", outfolder=sample_folder, args=args) # ArgParser and add_pypiper_args
 
     # Define target folder for converted files and implement the conversions; True=TF_Chipseq False=Histone_Chipseq
-    #target = os.path.join(os.path.dirname(args.input_file), file_name + ".bed")
-    target = args.output_file
 
-     
     print("Got input type: {}".format(args.input_type))
     print("Converting {} to BED format".format(args.input_file))
 
@@ -77,14 +67,14 @@ def main():
     width = "bdgbroadcall" if not args.chip_exp else "bdgpeakcall"
 
     # Call pyBigWig to ensure bigWig and bigBed files have the correct format
-    if args.input_type == "bigWig" or args.input_type == "bigBed":
+    if args.input_type in ["bigWig", "bigBed"]:
         big_check = pyBigWig.open(args.input_file)
 
     # if args.chip_exp:
     if args.input_type == "bedGraph":
-        cmd = bedGraph_template.format(input=args.input_file, output=target, width=width)
+        cmd = bedGraph_template.format(input=args.input_file, output=args.output_file, width=width)
     elif args.input_type == "bigWig" and big_check.isBigWig():
-        cmd = bigWig_template.format(input=args.input_file, output=target, width=width)
+        cmd = bigWig_template.format(input=args.input_file, output=args.output_file, width=width)
     elif args.input_type == "wig": 
         # get path to the genome config; from arg or env var if arg not provided
         refgenie_cfg_path = select_genome_config(filename=args.rfg_config, check_exist=False)
@@ -113,22 +103,19 @@ def main():
         print("Determined path to chrom.sizes asset: {}".format(chrom_sizes))
         # define a target for temporary bw files
         temp_target = os.path.join(sample_folder, file_id + ".bw")
-        cmd1 = wig_template.format(input=args.input_file, intermediate_bw=temp_target, chrom_sizes=chrom_sizes, width=width)
-        cmd2 = bigWig_template.format(input=temp_target, output=target, width=width)
-    elif args.input_type == "bigBed" and big_check.isBigBed():
-        cmd = bigBed_template.format(input=args.input_file, output=target)
-    elif args.input_type == "bed":
-        cmd = bed_template.format(input=args.input_file, output=target)
-    else:
-        raise NotImplementedError("Other conversions are not supported")
-
-    if args.input_type == "wig":
-        pm.run([cmd1, cmd2], target=target)
         pm.clean_add(temp_target)
-        pm.stop_pipeline()
-    else: 
-        pm.run(cmd, target=target)
-        pm.stop_pipeline()
+        cmd1 = wig_template.format(input=args.input_file, intermediate_bw=temp_target, chrom_sizes=chrom_sizes, width=width)
+        cmd2 = bigWig_template.format(input=temp_target, output=args.output_file, width=width)
+        cmd = [cmd1, cmd2]
+    elif args.input_type == "bigBed" and big_check.isBigBed():
+        cmd = bigBed_template.format(input=args.input_file, output=args.output_file)
+    elif args.input_type == "bed":
+        cmd = bed_template.format(input=args.input_file, output=args.output_file)
+    else:
+        raise NotImplementedError("'{}' format is not supported".format(args.input_type))
+
+    pm.run(cmd, target=args.output_file)
+    pm.stop_pipeline()
 
 
 if __name__ == '__main__':
